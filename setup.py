cfa4012510c4c182aa1d47898c9361ae5b0e12dc
Claire Cury <claire.cury.pro@gmail.com>
2016-05-05 10:39:18 +0100

    usable4graphVar branch: setup.py: adding the package for writting excel files in the install_requires field

--- setup.py
+++ setup.py
@@ -8,4 +8,5 @@ setup(name='niftybold',
       author_email='michael.hutel.13@ucl.ac.uk',
       license='MIT',
       packages=find_packages(),
+      install_requires=["numpy","scipy","sklearn","nibabel","nilearn","xlwt"],
       zip_safe=False)
